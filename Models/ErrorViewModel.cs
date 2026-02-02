0a8f3ef426267096041a68f0e3b7038d6c48df1f
omerbugra0909 <sakizciomerbugra@gmail.com>
2026-02-02 14:11:10 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ilk_uygulama_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
